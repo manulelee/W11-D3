21c1d5cb7cdbc14549ace9559056159b4f392711
manulelee <emanuele.sy@gmail.com>
2023-04-13 17:50:12 +0200

    fine(?)

--- src/components/ArticleDetails.tsx
+++ src/components/ArticleDetails.tsx
@@ -1,15 +1,13 @@
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-import { Button } from "react-bootstrap";
 import { Article } from "../interfaces/Article";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface SingleArticleProps {
   article: Article;
 }
 
 const ArticleDetails = (props: SingleArticleProps) => {
-  const navigate = useNavigate();
   return (
     <Col className="m-auto article-details">
       <Card style={{ width: "80%" }}>
@@ -18,7 +16,7 @@ const ArticleDetails = (props: SingleArticleProps) => {
           <Card.Title>{props.article.title}</Card.Title>
           <Card.Subtitle className="mb-2 text-mute">{props.article.publishedAt.toString()}</Card.Subtitle>
           <Card.Text className="cardText">{props.article.summary}</Card.Text>
-          <Link to={props.article.url}>
+          <Link to={props.article.url} target="blank">
             <div className="mx-2 btn btn-warning">Go to the page</div>
           </Link>
         </Card.Body>
